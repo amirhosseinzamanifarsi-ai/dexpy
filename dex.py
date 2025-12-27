4c338c6602b5f04b5b07a3101bc1f1c7884ab2e2
amirhosseinzamanifarsi-ai <amirhosseinzamanifarsi@gmail.com>
2025-12-27 22:40:20 +0330

    Update dex.py
    Signed-off-by: amirhosseinzamanifarsi-ai <amirhosseinzamanifarsi@gmail.com>

--- dex.py
+++ dex.py
@@ -17,7 +17,6 @@ def timing() :
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
-    options.add_argument("--remote-debugging-port=9222")  # مفید برای تشخیص مشکل در صورت نیاز
     path_geckodriver = '/usr/bin/geckodriver'
     service_path = Service(path_geckodriver)
     bot_ertebati = webdriver.Firefox(service=service_path,options=options)
