4e91eb94b1e98fc05454ea78b5609c15290a06dd
amirhosseinzamanifarsi-ai <amirhosseinzamanifarsi@gmail.com>
2025-12-27 22:34:12 +0330

    Update dex.py
    Signed-off-by: amirhosseinzamanifarsi-ai <amirhosseinzamanifarsi@gmail.com>

--- dex.py
+++ dex.py
@@ -1,3 +1,4 @@
+from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.common.by import By
 from selenium import webdriver
