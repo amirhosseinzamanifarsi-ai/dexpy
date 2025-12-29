30e4f31d01f0083b73d8fea3d651ae200ddc122d
amirhosseinzamanifarsi-ai <amirhosseinzamanifarsi@gmail.com>
2025-12-29 22:09:37 +0330

    Update dex.py
    Signed-off-by: amirhosseinzamanifarsi-ai <amirhosseinzamanifarsi@gmail.com>

--- dex.py
+++ dex.py
@@ -26,7 +26,7 @@ def timing() :
     time.sleep(8)
     wait = WebDriverWait(bot_ertebati, 20)
     wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '.ds-dex-table')))
-    #wait = WebDriverWait(bot_ertebati , 10)
+    wait = WebDriverWait(bot_ertebati , 10)
     data1 = bot_ertebati.find_element(By.CSS_SELECTOR , '.ds-dex-table')
     data_text = data1.text
     data_list = data_text.splitlines()
